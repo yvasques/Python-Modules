fdf5f81b0df063428d03bdca39aba5604300378e
Yann Vasques <yann.dias@outlook.com>
2026-08-27 09:42:04 -0300

    ex1 - identation fixed

--- Python-Module 04/ex1/ft_archive_creation.py
+++ Python-Module 04/ex1/ft_archive_creation.py
@@ -3,28 +3,43 @@
 import sys
 from typing import IO
 
+
 def transform_content(raw_content: str) -> str:
     lines: list[str] = raw_content.splitlines()
     transformed_lines: list[str] = []
     for line in lines:
         transformed_lines.append(f"{line}#")
-    return "\n".join(transformed_lines) + "\n" if lines else ""
+    if lines:
+        return "\n".join(transformed_lines) + "\n"
+    else:
+        return ""
 
 
 def save_to_file(destination_path: str, content: str) -> None:
-
-
+    file_obj: IO[str] | None = None
+    try:
+        print(f"Saving data to '{destination_path}'")
+        file_obj = open(destination_path, "w")
+        file_obj.write(content)
+        print(f"Data saved in file '{destination_path}'.")
+    except OSError as e:
+        print(f"Error saving file '{destination_path}': {e}")
+    finally:
+        if file_obj is not None and not file_obj.closed:
+            file_obj.close()
 
 
 def process_archive(file_path: str) -> None:
     print("=== Cyber Archives Recovery & Preservation ===")
     print(f"Accessing file '{file_path}'")
-    file_obj = IO[str] | None = None
+    file_obj: IO[str] | None = None
     raw_content: str = ""
     try:
         file_obj = open(file_path, "r")
         raw_content = file_obj.read()
+        print("---\n")
         print(raw_content, end="")
+        print("\n---")
     except OSError as e:
         print(f"Error opening file '{file_path}': {e}")
         return
@@ -34,8 +49,9 @@ def process_archive(file_path: str) -> None:
             print(f"File '{file_path}' closed.")
     transformed_content: str = transform_content(raw_content)
     print("\nTransform data:")
+    print("---\n")
     print(transformed_content, end="")
-
+    print("\n---")
     dest_file: str = input("Enter new file name (or empty): ").strip()
 
     if not dest_file:
@@ -43,12 +59,14 @@ def process_archive(file_path: str) -> None:
     else:
         save_to_file(dest_file, transformed_content)
 
+
 def main() -> None:
-    if sys.argv != 2:
-        print("Usage: ft_archive_creation.py <file")
+    if len(sys.argv) < 2:
+        print("Usage: ft_archive_creation.py <file>")
         return
     file_path: str = sys.argv[1]
     process_archive(file_path)
+
 
 if __name__ == "__main__":
     main()
